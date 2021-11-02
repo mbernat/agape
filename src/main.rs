8470622781500bcf1433d41295042200ad8850e8
Marek Bernát <marek.bernat@gmail.com>
2021-11-02 18:10:01 +0100

    Render simple stuff

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use bevy::prelude::*;
 
-fn setup(mut commands: Commands) {
-    commands.spawn().insert(Counter(0));
+struct Square;
+
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite::new(Vec2::new(1., 1.)),
+            material: materials.add(ColorMaterial::color([0.5, 0.2, 0.5, 1.0].into())),
+            transform: Transform::from_translation([0., 0., -100.].into()),
+            ..Default::default()
+        })
+        .insert(Square);
+    commands.spawn_bundle(PerspectiveCameraBundle::new_3d());
 }
 
-struct Counter(u32);
-
-fn increment(mut query: Query<&mut Counter>) {
-    for mut counter in query.iter_mut() {
-        counter.0 += 1;
-        println!("{}", counter.0)
+fn increment(mut query: Query<&mut Transform, With<Square>>) {
+    for mut transform in query.iter_mut() {
+        transform.translation.x += 0.1;
     }
 }
 
 fn main() {
     AppBuilder::default()
-    .add_plugins(DefaultPlugins)
-    .add_startup_system(setup.system())
-    .add_system(increment.system())
-    .run();
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .add_system(increment.system())
+        .run();
 }
